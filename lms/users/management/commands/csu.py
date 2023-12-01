43b1a4e8354d887d941f8dc205b5dc5a943b873d
lio <121868806+Lio-Kay@users.noreply.github.com>
2023-12-01 16:31:38 +0700

    feat: created command to create admin user

---
+++ lms/users/management/commands/csu.py
@@ -0,0 +1,20 @@
+from django.core.management import BaseCommand
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        user = User.objects.create_superuser(
+            email='admin@admin.com',
+            first_name='Admin',
+            last_name='Admin',
+            phone='+1234567890',
+            city='City',
+        )
+        user.set_password('1234')
+        user.save()
