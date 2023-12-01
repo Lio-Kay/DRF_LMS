562b43ba6c86a902b4746666e748e7916671e58a
lio <121868806+Lio-Kay@users.noreply.github.com>
2023-12-01 08:19:10 +0700

    feat: created custom user model

--- lms/users/models.py
+++ lms/users/models.py
@@ -1,3 +1,83 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator, validate_image_file_extension
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class CustomUserManager(BaseUserManager):
+    """Кастомная модель для создания пользователя"""
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault('is_active', True)
+        return self._create_user(email=email, password=password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self._create_user(email=email, password=password, **extra_fields)
+
+
+class User(AbstractUser):
+    """Кастомная модель пользователя"""
+
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='Email')
+    first_name = models.CharField(max_length=50, verbose_name='Имя')
+    last_name = models.CharField(max_length=50, verbose_name='Фамилия')
+    age = models.PositiveSmallIntegerField(verbose_name='Возраст',
+        validators=[
+            MinValueValidator(12),
+            MaxValueValidator(120)
+        ]
+    )
+    gender_choices = [
+        ('MALE', 'Мужчина'),
+        ('FEMALE', 'Женщина'),
+        ('OTHER', 'Предпочитаю не указывать'),
+    ]
+    gender = models.CharField(max_length=6, choices=gender_choices, validators='Гендер')
+    phone = PhoneNumberField(**NULLABLE, verbose_name='Телефон')
+    city = models.CharField(**NULLABLE, max_length=100, verbose_name='Город')
+    avatar = models.ImageField(upload_to='users/avatars/', **NULLABLE, verbose_name='Аватар',
+        validators=[
+            validate_image_file_extension
+        ]
+    )
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.email, self.first_name, self.last_name}'
+
+    class Meta:
+        verbose_name = 'пользователя'
+        verbose_name_plural = 'пользователи'
+        ordering = 'email',
