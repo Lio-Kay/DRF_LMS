54dae4bfcaa083c0e811da330531e138c0ab173e
lio <121868806+Lio-Kay@users.noreply.github.com>
2023-12-02 07:56:06 +0700

    feat: email validation to create method fix: deleter gender default value from create

--- lms/users/views.py
+++ lms/users/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model
-
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
 
 from users.serializers import UserOwnerSerializer, UserViewOnlySerializer
 from users.permissions import IsOwner
@@ -15,13 +16,21 @@ class UserCreateListAPIView(generics.ListCreateAPIView):
 
     def create(self, request, *args, **kwargs):
         data = request.data
+        email = data.get('email')
+        try:
+            validate_email(email)
+            # TODO
+            # Add email blacklisting
+        except ValidationError:
+            return Response({'Ошибка': 'Недопустимый адрес электронной почты'}, status=status.HTTP_400_BAD_REQUEST)
+
         user = User.objects.create_user(
             email=data.get('email'),
             password=data.get('password'),
             first_name=data.get('first_name'),
             last_name=data.get('last_name'),
             age=data.get('age'),
-            gender=data.get('gender', 'OTHER'),
+            gender=data.get('gender'),
             phone=data.get('phone'),
             city=data.get('city'),
             avatar=data.get('avatar'),
