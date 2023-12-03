c542589dd4c67a68db9112c9e5d77396ad5dda9c
lio <121868806+Lio-Kay@users.noreply.github.com>
2023-12-03 22:22:34 +0700

    fix: fixed urls

--- lms/education/urls.py
+++ lms/education/urls.py
@@ -10,8 +10,8 @@ app_name = EducationConfig.name
 
 urlpatterns = [
     path('sections/', SectionsListAPIView.as_view(), name='sections_list'),
-    path('section/<int:pk>/ ', SectionsRetrieveAPIView.as_view(), name='section_detail'),
-    path('materials/ ', MaterialsListAPIView.as_view(), name='materials_list'),
-    path('material/<int:pk>/ ', MaterialsRetrieveAPIView.as_view(), name='material_detail'),
-    path('material/<int:pk>/test/ ', StartTest.as_view(), name='material_test'),
+    path('sections/<int:pk>/', SectionsRetrieveAPIView.as_view(), name='section_detail'),
+    path('materials/', MaterialsListAPIView.as_view(), name='materials_list'),
+    path('materials/<int:pk>/', MaterialsRetrieveAPIView.as_view(), name='material_detail'),
+    path('materials/<int:pk>/test/', StartTest.as_view(), name='material_test'),
 ]
