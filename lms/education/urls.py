71e24c4a279c80365af66c98bd5c2a92c922e23a
lio <121868806+Lio-Kay@users.noreply.github.com>
2023-12-03 15:59:13 +0700

    fix: fixed views imports

--- lms/education/urls.py
+++ lms/education/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from education.apps import EducationConfig
 from education.views import (SectionsListAPIView, SectionsRetrieveAPIView,
-                             MaterialListSerializer, MaterialRetrieveSerializer,
+                             MaterialsListAPIView, MaterialsRetrieveAPIView,
                              StartTest)
 
 
@@ -11,7 +11,7 @@ app_name = EducationConfig.name
 urlpatterns = [
     path('sections/', SectionsListAPIView.as_view(), name='sections_list'),
     path('section/<int:pk>/ ', SectionsRetrieveAPIView.as_view(), name='section_detail'),
-    path('materials/ ', MaterialListSerializer.as_view(), name='materials_list'),
-    path('material/<int:pk>/ ', MaterialRetrieveSerializer.as_view(), name='material_detail'),
+    path('materials/ ', MaterialsListAPIView.as_view(), name='materials_list'),
+    path('material/<int:pk>/ ', MaterialsRetrieveAPIView.as_view(), name='material_detail'),
     path('material/<int:pk>/test/ ', StartTest.as_view(), name='material_test'),
 ]
