958a37ea8e06a2704b28d5035c930d9c9f3e2ef6
lio <121868806+Lio-Kay@users.noreply.github.com>
2023-12-03 15:23:33 +0700

    feat: wrote education views

--- lms/education/views.py
+++ lms/education/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.http import Http404
 
-# Create your views here.
+from education.serializers import (SectionListSerializer, SectionRetrieveSerializer,
+                                   MaterialListSerializer, MaterialRetrieveSerializer,
+                                   TestQuestionSerializer)
+from education.models import Section, Material, Test
+
+
+class SectionsListAPIView(generics.ListAPIView):
+    queryset = Section.objects.all()
+    serializer_class = SectionListSerializer
+
+
+class SectionsRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = Section.objects.all()
+    serializer_class = SectionRetrieveSerializer
+
+
+class MaterialsListAPIView(generics.ListAPIView):
+    queryset = Material.objects.all()
+    serializer_class = MaterialListSerializer
+
+
+class MaterialsRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = Material.objects.all()
+    serializer_class = MaterialRetrieveSerializer
+
+
+class StartTest(APIView):
+
+    def get(self, request, material_pk):
+        try:
+            test = Test.objects.get(material__pk=material_pk)
+        except Test.DoesNotExist:
+            raise Http404
+        serializer = TestQuestionSerializer(test.question.all(),many=True)
+        return Response(serializer)
