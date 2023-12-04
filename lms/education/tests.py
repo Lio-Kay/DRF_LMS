010c5d632a535f59fc44f2d404f82c2f7ba541c5
lio <121868806+Lio-Kay@users.noreply.github.com>
2023-12-05 01:38:33 +0700

    feat: added test media file feat: made dumpdata

--- lms/education/tests.py
+++ lms/education/tests.py
@@ -30,7 +30,6 @@ class MediaModelTests(TestCase):
         with self.assertRaises(ValidationError):
             media.clean()
 
-# Create your tests here.
         media.local_image = 'path/to/image.jpg'
         media.external_video = 'https://example.com/video.mp4'
         with self.assertRaises(ValidationError):
@@ -60,4 +59,169 @@ class MediaModelTests(TestCase):
         media = Media.objects.create(name='Test_Media')
         self.assertIsNotNone(media.creation_date)
         self.assertEqual(media.creation_date.date(), timezone.now().date())
+
+
+class SectionModelTests(TestCase):
+    def setUp(self):
+        self.media = Media.objects.create(name='Test_Media')
+        self.section = Section.objects.create(
+            name='Test_Section',
+            description='Test_Description',
+            status='OPEN',
+            creation_date=timezone.now(),
+            last_update=timezone.now(),
+            base_price=100,
+        )
+        self.section.media.add(self.media)
+
+    def tearDown(self):
+        self.section.delete()
+        self.media.delete()
+
+    def test_create_section(self):
+        section_count = Section.objects.count()
+        self.assertEqual(section_count, 1)
+        self.assertEqual(self.section.name, 'Test_Section')
+        self.assertEqual(self.section.description, 'Test_Description')
+        self.assertEqual(self.section.status, 'OPEN')
+        self.assertAlmostEqual(self.section.creation_date, timezone.now(),
+                               delta=timedelta(seconds=1))
+        self.assertAlmostEqual(self.section.last_update, timezone.now(),
+                               delta=timedelta(seconds=1))
+        self.assertEqual(self.section.base_price.amount, 100)
+        self.assertIn(self.media, self.section.media.all())
+
+    def test_status_choices_auto_set(self):
+        section = Section.objects.create(name='Test_Section')
+        self.assertEqual(section.status, 'CLOSED')
+
+    def test_last_update_after_creation_date(self):
+        with self.assertRaises(IntegrityError):
+            with transaction.atomic():
+                Section.objects.create(
+                    name='Test_Section',
+                    creation_date=timezone.now(),
+                    last_update=timezone.now() - timedelta(days=1)
+                )
+
+    def test_save_method(self):
+        section = Section.objects.create(name='Test_Section')
+        self.assertIsNotNone(section.creation_date)
+        self.assertEqual(section.creation_date.date(), timezone.now().date())
+        section = Section.objects.create(name='Test_Section')
+        self.assertIsNotNone(section.last_update)
+        self.assertEqual(section.last_update.date(), timezone.now().date())
+
+
+class MaterialModelTests(TestCase):
+    def setUp(self):
+        self.section = Section.objects.create(name='Test_Section')
+        self.media = Media.objects.create(name='Test_Media')
+        self.material = Material.objects.create(
+            name='Test_Material',
+            text='Test_Text',
+            status='CLOSED',
+            creation_date=timezone.now(),
+            last_update=timezone.now(),
+            section=self.section
+        )
+        self.material.media.add(self.media)
+
+    def tearDown(self):
+        self.material.delete()
+        self.section.delete()
+        self.media.delete()
+
+    def test_create_material(self):
+        material_count = Material.objects.count()
+        self.assertEqual(material_count, 1)
+        self.assertEqual(self.material.name, 'Test_Material')
+        self.assertEqual(self.material.text, 'Test_Text')
+        self.assertEqual(self.material.status, 'CLOSED')
+        self.assertAlmostEqual(self.material.creation_date, timezone.now(),
+                               delta=timedelta(seconds=1))
+        self.assertAlmostEqual(self.material.last_update, timezone.now(),
+                               delta=timedelta(seconds=1))
+        self.assertEqual(self.material.section, self.section)
+        self.assertIn(self.media, self.material.media.all())
+
+    def test_status_choices_auto_set(self):
+        material = Section.objects.create(name='Test_Material')
+        self.assertEqual(material.status, 'CLOSED')
+
+    def test_last_update_after_creation_date(self):
+        with self.assertRaises(IntegrityError):
+            with transaction.atomic():
+                Material.objects.create(
+                    name='Test_Material',
+                    creation_date=timezone.now(),
+                    last_update=timezone.now() - timedelta(days=1)
+                )
+
+    def test_clean_method(self):
+        section = Section.objects.create(
+            name='Test_Section',
+            status='ARCHIVED')
+        material = Material(
+            name='Test_Material',
+            status='OPEM',
+            section=section)
+        with self.assertRaises(ValidationError):
+            material.clean()
+
+        section.status = 'ARCHIVED'
+        section.save()
+        material.status = 'OPEN'
+        with self.assertRaises(ValidationError):
+            material.clean()
+
+        material.status = 'ARCHIVED'
+        material.clean()
+
+    def test_save_method(self):
+        material = Material.objects.create(name='Test_Material')
+        self.assertIsNotNone(material.creation_date)
+        self.assertEqual(material.creation_date.date(), timezone.now().date())
+        material = Material.objects.create(name='Test_Material')
+        self.assertIsNotNone(material.last_update)
+        self.assertEqual(material.last_update.date(), timezone.now().date())
+
+
+class TestAnswerModelTests(TestCase):
+    def setUp(self):
+        self.answer = TestAnswer.objects.create(answer=1)
+
+    def tearDown(self):
+        self.answer.delete()
+
+    def test_create_answer(self):
+        answer_count = TestAnswer.objects.count()
+        self.assertEqual(answer_count, 1)
+        self.assertEqual(self.answer.answer, 1)
+
+
+class TestQuestionModelTests(TestCase):
+    def setUp(self):
+        self.answer1 = TestAnswer.objects.create(answer=1)
+        self.answer2 = TestAnswer.objects.create(answer=2)
+        self.media = Media.objects.create(name='Test_Media')
+        self.testquestion = TestQuestion.objects.create(
+            question='Test_Question',
+            answer=self.answer1)
+        self.testquestion.choices.add(self.answer1)
+        self.testquestion.choices.add(self.answer2)
+        self.testquestion.media.add(self.media)
+
+    def tearDown(self):
+        self.answer1.delete()
+        self.answer2.delete()
+        self.media.delete()
+        self.testquestion.delete()
+
+    def test_question_creation(self):
+        question_count = TestQuestion.objects.count()
+        self.assertEqual(question_count, 1)
+        self.assertEqual(self.testquestion.question, 'Test_Question')
+        self.assertEqual(self.testquestion.answer, self.answer1)
+        self.assertIn(self.media, self.testquestion.media.all())
 
