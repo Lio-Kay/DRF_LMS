421cc3bf3f63b19740e98096416ba57b00fa3804
lio <121868806+Lio-Kay@users.noreply.github.com>
2023-12-08 17:32:46 +0700

    feat: set-up project wide pagination

--- lms/lms/settings.py
+++ lms/lms/settings.py
@@ -125,6 +125,9 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 # DRF
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'MAX_PAGE_SIZE': 100,
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
@@ -132,7 +135,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ),
 }
 SIMPLE_JWT = {
