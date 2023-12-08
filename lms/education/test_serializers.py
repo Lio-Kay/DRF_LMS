dd63818aa4422261cb7be32af4ee0d93dc39898e
lio <121868806+Lio-Kay@users.noreply.github.com>
2023-12-08 23:57:52 +0700

    feat: wrote tests for MaterialRetrieveSerializerTest

--- lms/education/test_serializers.py
+++ lms/education/test_serializers.py
@@ -88,3 +88,47 @@ class MaterialListSerializerTest(TestCase):
         media_links_serializer = MediaLinkSerializer(instance=self.material.media.all(), many=True)
         expected_media_links = media_links_serializer.data
         self.assertEqual(serializer.data['media_links'], expected_media_links)
+
+
+class MaterialRetrieveSerializerTest(TestCase):
+    def setUp(self):
+        self.section = Section.objects.create(name='Section_1')
+        self.media = Media.objects.create(name='Media_1')
+        self.material = Material.objects.create(
+            name='Material_1',
+            section=self.section,
+            status='OPEN',
+            text='Lorem ipsum dolor sit amet.',
+            creation_date=timezone.now(),
+            last_update=timezone.now(),
+        )
+        self.material.media.add(self.media)
+
+    def tearDown(self):
+        self.section.delete()
+        self.media.delete()
+        self.material.delete()
+
+    def test_get_section(self):
+        serializer = MaterialRetrieveSerializer(instance=self.material)
+        expected_section = 'Section_1'
+        self.assertEqual(serializer.data['section'], expected_section)
+
+    def test_get_media_names(self):
+        serializer = MaterialRetrieveSerializer(instance=self.material)
+        expected_media_name = 'Media_1'
+        self.assertEqual(serializer.data['media_names'], expected_media_name)
+
+    def test_meta_fields(self):
+        serializer = MaterialRetrieveSerializer()
+        expected_fields = ('name', 'section', 'status', 'text',
+                           'creation_date', 'last_update',
+                           'media_names', 'media_links',)
+        self.assertEqual(serializer.Meta.model, Material)
+        self.assertEqual(serializer.Meta.fields, expected_fields)
+
+    def test_media_links_serializer(self):
+        serializer = MaterialRetrieveSerializer(instance=self.material)
+        media_links_serializer = MediaLinkSerializer(instance=self.material.media.all(), many=True)
+        expected_media_links = media_links_serializer.data
+        self.assertEqual(serializer.data['media_links'], expected_media_links)
