d5bc15e20e097032b37cb742028a716c3c6024f4
lio <121868806+Lio-Kay@users.noreply.github.com>
2023-12-12 03:35:33 +0700

    feat: deleted old user auth app

--- lms/users/permissions.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-
-class IsOwner(BasePermission):
-
-    def has_permission(self, request, view):
-        return request.user.pk == view.get_object().pk
