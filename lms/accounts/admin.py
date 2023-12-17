3198eb4d6730cf7e8aba2a458015fd461b8258ec
lio <121868806+Lio-Kay@users.noreply.github.com>
2023-12-17 12:21:31 +0700

    feat: added user model to admin

--- lms/accounts/admin.py
+++ lms/accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from accounts.models import CustomUser
+
+
+@admin.register(CustomUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'first_name', 'last_name', 'age', 'gender',
+                    'phone', 'city',)
+    list_display_links = 'id', 'email', 'first_name',
+    list_filter = 'gender', 'city',
+    search_fields = 'last_name', 'age', 'gender', 'phone', 'city',
+    list_editable = 'last_name', 'age', 'gender', 'phone', 'city',
