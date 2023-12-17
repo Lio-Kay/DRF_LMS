7d28620ebd7c4b390e1a751b667f1de712f66488
lio <121868806+Lio-Kay@users.noreply.github.com>
2023-12-17 16:14:19 +0700

    feat: fixed urls for dj rest auth

--- lms/lms/urls.py
+++ lms/lms/urls.py
@@ -35,6 +35,7 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
 
-    path('api/v1/accounts/', include('accounts.urls', namespace='accounts')),
+    path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('api/v1/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('api/v1/education/', include('education.urls', namespace='education'))
 ]
