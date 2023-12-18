928e668fdb54aa968c0591d1d29116aa7f028fa3
lio <121868806+Lio-Kay@users.noreply.github.com>
2023-12-18 15:15:04 +0700

    feat: wrote VerifyEmailAPIView to be extended

--- lms/accounts/views.py
+++ lms/accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+import requests
+from django.conf import settings
+from django.shortcuts import redirect
+from django.contrib.auth import get_user_model
+from rest_framework.views import APIView
 
-# Create your views here.
+# Get the UserModel
+UserModel = get_user_model()
+
+
+class VerifyEmailAPIView(APIView):
+    def get(self, request, key, *args, **kwargs):
+        protocol = 'https://' if request.is_secure() else 'http://'
+        web_url = protocol + request.get_host()
+        post_url = web_url + "/api/v1/dj-rest-auth/registration/verify-email/"
+        redirect_url = settings.LOGIN_URL
+        post_data = {'key': key}
+        result = requests.post(post_url, data=post_data)
+        return redirect(redirect_url)
