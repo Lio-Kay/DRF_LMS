7d50f7a26532d2cac3dccaa4ad3effeace64211b
lio <121868806+Lio-Kay@users.noreply.github.com>
2023-12-22 23:32:28 +0700

    feat: added link to drf settings

--- lms/lms/settings.py
+++ lms/lms/settings.py
@@ -137,6 +137,7 @@ SITE_ID = 1
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # DRF
+# https://www.django-rest-framework.org/api-guide/settings/
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
