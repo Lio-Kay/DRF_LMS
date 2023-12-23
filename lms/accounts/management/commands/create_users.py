ecab7943adb56190c8ae95ce2597817a8e122209
lio <121868806+Lio-Kay@users.noreply.github.com>
2023-12-24 06:37:16 +0700

    feat: added command to fill db with users

---
+++ lms/accounts/management/commands/create_users.py
@@ -0,0 +1,44 @@
+from django.core.management import BaseCommand
+from django.contrib.auth import get_user_model
+from faker import Faker
+
+
+fake = Faker('ru_RU')
+User = get_user_model()
+
+
+class Command(BaseCommand):
+    """Команда для удаления всех пользователей, кроме персонала и заполнения
+    БД при помощи библиотеки Faker"""
+
+    help = ('Удаляет всех пользователей, кроме персонала, и заполняет БД '
+            'новыми пользователями.')
+
+    def handle(self, *args, **options):
+
+        number_of_users = input('Какое кол-во пользователей добавить?\n')
+        try:
+            number_of_users = int(number_of_users)
+        except ValueError:
+            self.stderr.write(self.style.ERROR('Нечисловое значение'))
+            exit()
+
+        User.objects.filter(is_staff=False).delete()
+
+        for user_num in range(number_of_users):
+            user = User.objects.create(
+                email=fake.email(),
+                first_name=fake.first_name(),
+                last_name=fake.last_name(),
+                age=fake.random_int(min=12, max=120),
+                gender=fake.random_element(['MALE', 'FEMALE', 'OTHER']),
+                phone=fake.phone_number(),
+                city=fake.city(),
+                avatar='/path_to_default_avatar.jpg',
+            )
+            user.set_password('1234')
+            user.save()
+
+        self.stdout.write(self.style.SUCCESS(
+            f'Удалили всех пользователей, кроме персонала, и заполнили БД '
+            f'{number_of_users} новыми пользователями.'))
