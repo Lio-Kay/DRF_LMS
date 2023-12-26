dfaf938123fdfb5d7f28d117c72616b0b92064d7
lio <121868806+Lio-Kay@users.noreply.github.com>
2023-12-26 08:28:43 +0700

    feat: expanded command to fill every model with data

--- lms/education/management/commands/c_edudata.py
+++ lms/education/management/commands/c_edudata.py
@@ -17,8 +17,8 @@ class Command(BaseCommand):
 
     @staticmethod
     def get_creation_time():
-        return make_aware(fake.past_datetime(
-            start_date='-1y'))
+        return make_aware(fake.date_time_between(
+            start_date='-2y', end_date='-6M'))
 
     @staticmethod
     def get_update_time():
@@ -38,13 +38,11 @@ class Command(BaseCommand):
             name=fake.word(),
             creation_date=self.get_creation_time(),
             local_image=fake.file_name(extension='jpg'),
-            external_image=fake.image_url(),
             )
         media2 = Media.objects.create(
             name=fake.word(),
             creation_date=self.get_creation_time(),
-            local_video=fake.file_name(extension='mp4'),
-            external_video=fake.url()
+            external_video=fake.url(),
         )
         media3 = Media.objects.create(
             name=fake.word(),
@@ -81,6 +79,120 @@ class Command(BaseCommand):
             base_price=Money(fake.random_int(min=100, max=10000), 'RUB')
         )
         section3.media.add(media3)
+
+        # Material
+        Material.objects.all().delete()
+        material1 = Material.objects.create(
+            name=fake.word(),
+            text=fake.paragraph(),
+            status='ARCHIVED',
+            creation_date=self.get_creation_time(),
+            last_update=self.get_update_time(),
+        )
+        material1.section = section1
+        material1.media.add(media1)
+        material2 = Material.objects.create(
+            name=fake.word(),
+            text=fake.paragraph(),
+            status='ARCHIVED',
+            creation_date=self.get_creation_time(),
+            last_update=self.get_update_time(),
+        )
+        material2.section = section1
+        material2.media.add(media2)
+        material3 = Material.objects.create(
+            name=fake.word(),
+            text=fake.paragraph(),
+            status='CLOSED',
+            creation_date=self.get_creation_time(),
+            last_update=self.get_update_time(),
+        )
+        material3.section = section2
+        material3.media.add(media3)
+        material4 = Material.objects.create(
+            name=fake.word(),
+            text=fake.paragraph(),
+            status='CLOSED',
+            creation_date=self.get_creation_time(),
+            last_update=self.get_update_time(),
+        )
+        material4.section = section2
+        material4.media.add(media1)
+        material5 = Material.objects.create(
+            name=fake.word(),
+            text=fake.paragraph(),
+            status='OPEN',
+            creation_date=self.get_creation_time(),
+            last_update=self.get_update_time(),
+        )
+        material5.section = section3
+        material5.media.add(media2)
+        material6 = Material.objects.create(
+            name=fake.word(),
+            text=fake.paragraph(),
+            status='OPEN',
+            creation_date=self.get_creation_time(),
+            last_update=self.get_update_time(),
+        )
+        material6.section = section3
+        material6.media.add(media3)
+
+        # TestAnswer
+        TestAnswer.objects.all().delete()
+        testanswer1 = TestAnswer.objects.create(answer=fake.word())
+        testanswer2 = TestAnswer.objects.create(answer=fake.word())
+        testanswer3 = TestAnswer.objects.create(answer=fake.word())
+        testanswer4 = TestAnswer.objects.create(answer=fake.word())
+        testanswer5 = TestAnswer.objects.create(answer=fake.word())
+        testanswer6 = TestAnswer.objects.create(answer=fake.word())
+
+        # TestQuestion
+        TestQuestion.objects.all().delete()
+        testquestion1 = TestQuestion(
+            question=fake.paragraph(),
+            answer=testanswer1,
+        )
+        testquestion1.save()
+        testquestion1.choices.add(testanswer1, testanswer2,
+                                  testanswer3, testanswer4)
+        testquestion1.media.add(media1)
+        testquestion2 = TestQuestion(
+            question=fake.paragraph(),
+            answer=testanswer2,
+        )
+        testquestion2.save()
+        testquestion2.choices.add(testanswer2, testanswer3,
+                                  testanswer4, testanswer5)
+        testquestion2.media.add(media2)
+        testquestion3 = TestQuestion(
+            question=fake.paragraph(),
+            answer=testanswer3,
+        )
+        testquestion3.save()
+        testquestion3.choices.add(testanswer3, testanswer4,
+                                  testanswer5, testanswer6)
+        testquestion3.media.add(media3)
+
+        # Test
+        Test.objects.all().delete()
+        test1 = Test.objects.create(
+            material=material1,
+            creation_date=self.get_creation_time(),
+            last_update=self.get_update_time(),
+        )
+        test1.question.add(testquestion1)
+        test2 = Test.objects.create(
+            material=material2,
+            creation_date=self.get_creation_time(),
+            last_update=self.get_update_time(),
+        )
+        test2.question.add(testquestion2)
+        test3 = Test.objects.create(
+            material=material3,
+            creation_date=self.get_creation_time(),
+            last_update=self.get_update_time(),
+        )
+        test3.question.add(testquestion3)
 
         self.stdout.write(self.style.SUCCESS(
             f'Добавили данные для приложения education'))
