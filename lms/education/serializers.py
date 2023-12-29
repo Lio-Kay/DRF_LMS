5b4b5b4f748633bdfbd6f0f1a3d31ca9723924ed
lio <121868806+Lio-Kay@users.noreply.github.com>
2023-12-30 01:00:05 +0700

    feat: fixed MediaLinkSerializer logic in Material Serializers, Sections Serializers

--- lms/education/serializers.py
+++ lms/education/serializers.py
@@ -20,76 +20,56 @@ class MediaLinkSerializer(serializers.ModelSerializer):
 
 class MaterialListSerializer(serializers.ModelSerializer):
     section = serializers.SerializerMethodField('get_section')
-    media_names = serializers.SerializerMethodField('get_media_names')
     media_links = MediaLinkSerializer(source='media', many=True)
 
     def get_section(self, obj):
         return obj.section.name
-
-    def get_media_names(self, obj):
-        media_names = obj.media.values_list('name', flat=True)
-        return media_names
 
     class Meta:
         model = Material
         fields = ('name', 'section', 'status',
                   'creation_date', 'last_update',
-                  'media_names', 'media_links',)
+                  'media_links',)
 
 
 class MaterialRetrieveSerializer(serializers.ModelSerializer):
     section = serializers.SerializerMethodField('get_section')
-    media_names = serializers.SerializerMethodField('get_media_names')
     media_links = MediaLinkSerializer(source='media', many=True)
 
     def get_section(self, obj):
         return obj.section.name
 
-    def get_media_names(self, obj):
-        media_names = obj.media.values_list('name', flat=True)
-        return media_names
-
     class Meta:
         model = Material
         fields = ('name', 'section', 'status', 'text',
                   'creation_date', 'last_update',
-                  'media_names', 'media_links',)
+                  'media_links',)
 
 
 class SectionListSerializer(serializers.ModelSerializer):
     materials_count = serializers.IntegerField(source='material_section.all.count')
-    media_names = serializers.SerializerMethodField('get_media_names')
     media_links = MediaLinkSerializer(source='media', many=True)
-
-    def get_media_names(self, obj):
-        media_names = obj.media.values_list('name', flat=True)
-        return media_names
 
     class Meta:
         model = Section
         fields = ('name', 'status', 'creation_date', 'last_update',
                   'materials_count', 'base_price',
-                  'media_names', 'media_links',)
+                  'media_links',)
 
 
 class SectionRetrieveSerializer(serializers.ModelSerializer):
     materials = serializers.SerializerMethodField('get_materials_names')
-    media_names = serializers.SerializerMethodField('get_media_names')
     media_links = MediaLinkSerializer(source='media', many=True)
 
     def get_materials_names(self, obj):
         materials_names = obj.material_section.values_list('name', flat=True)
         return materials_names
 
-    def get_media_names(self, obj):
-        media_names = obj.media.values_list('name', flat=True)
-        return media_names
-
     class Meta:
         model = Section
         fields = ('name', 'status', 'description',
                   'creation_date', 'last_update', 'base_price',
-                  'materials', 'media_names', 'media_links',)
+                  'materials', 'media_links',)
 
 
 class TestAnswerSerializer(serializers.ModelSerializer):
