1d8deaf89eba6b92caaf297b13cfec8fbfa4bd81
lio <121868806+Lio-Kay@users.noreply.github.com>
2023-12-31 09:42:20 +0700

    feat: created urls for TBA, JWT auth

--- lms/accounts/urls.py
+++ lms/accounts/urls.py
@@ -6,4 +6,6 @@ app_name = AccountsConfig.name
 
 urlpatterns = [
     path('', include('djoser.urls')),
+    path('', include('djoser.urls.authtoken')),
+    path('', include('djoser.urls.jwt')),
 ]
