0ca08df1b1a80d17618bf261ff45c1b0e7d20c44
lio <121868806+Lio-Kay@users.noreply.github.com>
2024-01-02 06:05:21 +0700

    feat: improved code logic feat: improved docstring

--- lms/accounts/management/commands/c_susr.py
+++ lms/accounts/management/commands/c_susr.py
@@ -1,33 +1,44 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import check_password
 from django.core.management import BaseCommand
 
 User = get_user_model()
 
 
 class Command(BaseCommand):
-    """Команда для создания суперпользователя"""
-    help = ('Создает суперпользователя. Email: admin@admin.com, '
-            'Phone: +1234567890, Password: admin')
+    """
+    Команда для создания суперпользователя
+
+    email=admin@admin.com,
+    first_name=Admin,
+    last_name=Admin,
+    phone=+1234567890,
+    password=admin
+    """
+    help = ('Создает суперпользователя.\n'
+            'Email: admin@admin.com, Телефон: +12345678900, Пароль: admin')
 
     def handle(self, *args, **options):
-        if User.objects.filter(email='admin@admin.com'):
-            self.stderr.write(
-                self.style.ERROR('Суперпользователь уже существует. '
-                                 'Email: admin @ admin.com, '
-                                 'Phone: +1234567890, Password: admin')
-            )
-            exit()
+        superuser = User.objects.get(email='admin@admin.com')
+        correct_password = 'admin'
+        if superuser:
+            if check_password(correct_password, superuser.password):
+                self.stderr.write(
+                    self.style.ERROR('Суперпользователь уже существует')
+                )
+                exit()
+            User.objects.get(email='admin@admin.com').delete()
 
         user = User.objects.create_superuser(
             email='admin@admin.com',
             first_name='Admin',
             last_name='Admin',
-            phone='+1234567890',
+            phone='+12345678900',
         )
         user.set_password('admin')
         user.save()
 
         self.stdout.write(self.style.SUCCESS(
-            'Создали суперпользователя. Email: admin@admin.com, '
-            'Phone: +1234567890, Password: admin')
+            'Создали суперпользователя.\n'
+            'Email: admin@admin.com, Телефон: +12345678900, Пароль: admin')
         )
