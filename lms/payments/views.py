ec2d3ae4e84bc47098171246ddd954b52f78cfa5
lio <121868806+Lio-Kay@users.noreply.github.com>
2024-01-12 03:19:09 +0700

    feat: created UserPayment views

--- lms/payments/views.py
+++ lms/payments/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.exceptions import NotFound
 
-# Create your views here.
+from payments.models import Payment
+from payments.permissions import IsOwner
+from payments.serializers import PaymentSerializer
+
+
+class UserPaymentListAPIView(generics.ListAPIView):
+    serializer_class = PaymentSerializer
+    permission_classes = [IsOwner]
+
+    def get_queryset(self):
+        user_id = self.kwargs['user_id']
+        return Payment.objects.filter(user_id=user_id)
+
+
+class UserPaymentDetailAPIView(generics.RetrieveAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+    permission_classes = [IsOwner]
+
+    def get_object(self):
+        user_id = self.kwargs['user_id']
+        payment_id = self.kwargs['payment_id']
+        try:
+            return Payment.objects.get(id=payment_id, user_id=user_id)
+        except Payment.DoesNotExist:
+            raise NotFound('Платеж не найден')
