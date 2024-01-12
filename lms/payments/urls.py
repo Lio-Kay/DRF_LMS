3e8c3f8286d66f6ca70fbf158617cdd44c4fc20e
lio <121868806+Lio-Kay@users.noreply.github.com>
2024-01-13 01:05:03 +0700

    fix: url paths for UserPayment

--- lms/payments/urls.py
+++ lms/payments/urls.py
@@ -6,8 +6,8 @@ from payments.views import UserPaymentListAPIView, UserPaymentDetailAPIView
 app_name = PaymentsConfig.name
 
 urlpatterns = [
-    path('payments/<int:user_id>', UserPaymentListAPIView.as_view(),
+    path('<int:user_id>/', UserPaymentListAPIView.as_view(),
          name='user_payments_list'),
-    path('payments/<int:user_id>/<int:payment_id>/', UserPaymentDetailAPIView.as_view(),
+    path('<int:user_id>/<int:payment_id>/', UserPaymentDetailAPIView.as_view(),
          name='user_payment_detail'),
 ]
