3829132ec4448bacbc74e3714c8e1cc3cec1b394
lio <121868806+Lio-Kay@users.noreply.github.com>
2024-01-17 04:40:00 +0700

    feat: updated expiration_month validators

--- lms/payments/models.py
+++ lms/payments/models.py
@@ -2,7 +2,8 @@ import re
 
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
-from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.core.validators import (MinLengthValidator, MaxLengthValidator,
+                                    MinValueValidator, MaxValueValidator)
 from django.db import models
 from django.utils import timezone
 import calendar
@@ -76,6 +77,7 @@ class UserCardData(models.Model):
         verbose_name='Номер карты')
     owner_name = models.CharField(max_length=100, verbose_name='Владелец')
     expiration_month = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(12)],
         verbose_name='Месяц действия')
     expiration_year = models.PositiveSmallIntegerField(
         verbose_name='Год действия')
